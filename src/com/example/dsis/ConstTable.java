44e8eb887764c4957b64e54c50747ddbb88ff37e
DSIS <wjdthtjfltm@gmail.com>
2015-01-14 14:20:01 +0900

    web Login from

--- src/com/example/dsis/ConstTable.java
+++ src/com/example/dsis/ConstTable.java
@@ -24,4 +24,23 @@ public class ConstTable {
         public static final String GRAY = "#D8D8D8";
         public static final String PINK = "#FFD6FF";
     }
+    
+    public static class ID{
+    	public static final String ID = "txtStudentCd";
+    	public static final String PW = "txtPasswd";
+    	public static final String VIEW_STATE = "__VIEWSTATE";
+    	public static final String LOGIN_BTN_X = "ibtnLogin.x";
+    	public static final String LOGIN_BTN_Y = "ibtnLogin.y";
+    }
+    
+    public static class VALUE{
+    	public static final String VIEW_STATE_VAL = "dDwxNjcyNzcxNDU2O3Q8O2w8aTwwPjs+O2w8dDw7bDxpPDExPjtpPDEzPjs+O2w8dDxwPHA8bDxOYXZpZ2F0ZVVybDs+O2w8amF2YXNjcmlwdDp3aW5PcGVuKCk7Pj47Pjs7Pjt0PHA8O3A8bDxvbmNsaWNrOz47bDxqYXZhc2NyaXB0OmZybUxvZ2luLnJlc2V0KClcO3JldHVybiBmYWxzZVw7Oz4+Pjs7Pjs+Pjs+PjtsPGlidG5Mb2dpbjtJbWFnZUJ1dHRvbjE7Pj5ImIpoeshh3TEd2zfLXcmtEp7woA==";
+    	public static final String LOG_IN_BTN_VAL = "0";
+    }
+    
+    public static class URL{
+    	public static final String LOG_IN = "http://student.donga.ac.kr/Login.aspx";
+    	public static final String TIME_TABLE = "";
+    	public static final String STUDENT_INFO = "";
+    }
 }
